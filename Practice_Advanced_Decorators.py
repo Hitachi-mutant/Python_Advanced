87a0bfd12c0afb60bed6d554b149b86ed47f9273
Alex <goldskydiver@gmail.com>
2023-07-13 22:11:05 +0300

    Practice_Advanced_Decorators tasks are finished

--- Practice_Advanced_Decorators.py
+++ Practice_Advanced_Decorators.py
@@ -1,102 +1,102 @@
 # 1. Реалізувати декоратор, який перевіряє, чи є аргумент функції числом 
 #  та виводить повідомлення про помилку, якщо це не так.
 
-# def decorator_to_check_args(our_function_to_check):
-#     def wrapper_function(*args):
-#         for element in args:
-#             if isinstance(element, str) and element.isdigit():
-#                 print(f"{element} is a digit.")
-#             elif isinstance(element, int):
-#                 print(f"{element} is a digit.")
-#             else:
-#                 print(f"{element} is not a digit.")
-#         result = our_function_to_check(*args)
-#         return result
-#     return wrapper_function
+def decorator_to_check_args(our_function_to_check):
+    def wrapper_function(*args):
+        for element in args:
+            if isinstance(element, str) and element.isdigit():
+                print(f"{element} is a digit.")
+            elif isinstance(element, int):
+                print(f"{element} is a digit.")
+            else:
+                print(f"{element} is not a digit.")
+        result = our_function_to_check(*args)
+        return result
+    return wrapper_function
 
-# @decorator_to_check_args
-# def func_with_the_numbers(*args):
-#     return args
+@decorator_to_check_args
+def func_with_the_numbers(*args):
+    return args
 
-# output = func_with_the_numbers('a', 1, '2', 0, True, None)
-# print(output)
+output = func_with_the_numbers('a', 1, '2', 0, True, None)
+print(output)
 
 
 # 2. Написати декоратор, який обмежує кількість разів виклику 
 #  функції та видає помилку, якщо ліміт перевищено.
 
-# def decorator(func):
-#     calls = 0
-#     def wrapper(*args):
-#         nonlocal calls
-#         calls += 1
-#         if calls <= 3:
-#             return func(*args)
-#         else:
-#             print("You can't call your function more than three times.")
-#             return None  # Return None when the limit is exceeded
+def decorator(func):
+    calls = 0
+    def wrapper(*args):
+        nonlocal calls
+        calls += 1
+        if calls <= 3:
+            return func(*args)
+        else:
+            print("You can't call your function more than three times.")
+            return None  # Return None when the limit is exceeded
 
-#     return wrapper
+    return wrapper
 
-# @decorator
-# def function(name_1, name_2):
-#     return f"{name_1} and {name_2}\n"
+@decorator
+def function(name_1, name_2):
+    return f"{name_1} and {name_2}\n"
 
-# for _ in range(5):
-#     result = function("A", "B")
-#     print(result)
+for _ in range(5):
+    result = function("A", "B")
+    print(result)
 
 
 # 3. Створити декоратор, який змінює результат функції на протилежне значення (наприклад, з True на False).
 
-# def change_func(func_as_arg):
-#     def wrapper_function(*args):
-#             result = func_as_arg(*args)
-#             if result:
-#                 return False
-#             else: 
-#                 return True
-#     return wrapper_function
+def change_func(func_as_arg):
+    def wrapper_function(*args):
+            result = func_as_arg(*args)
+            if result:
+                return False
+            else: 
+                return True
+    return wrapper_function
 
-# @change_func
-# def my_func(a):
-#     if a > 0:
-#         return True
-#     else: return False
+@change_func
+def my_func(a):
+    if a > 0:
+        return True
+    else: return False
 
-# print(my_func(0))
+print(my_func(0))
 
 
 # 4. Напишіть декоратор, який перевіряє типи аргументів, переданих до функції, 
 #  та виводить повідомлення про помилку, якщо типи не відповідають очікуваним.
 
-# def check_arguments(func_as_arg):
-#     def wrapper_function(*args):
-#         lets_check = func_as_arg(*args)
-#         valid_items = []
-#         for item in lets_check:
-#             if isinstance(item, (bool, int, str, float)):
-#                 valid_items.append(item)
-#             else:
-#                 pass
-#         return valid_items
-#     return wrapper_function
+def check_arguments(func_as_arg):
+    def wrapper_function(*args):
+        lets_check = func_as_arg(*args)
+        valid_items = []
+        for item in lets_check:
+            if isinstance(item, (bool, int, str, float)):
+                valid_items.append(item)
+            else:
+                pass
+        return valid_items
+    return wrapper_function
 
-# @check_arguments
-# def function_with_logic(*args):
-#     return args
+@check_arguments
+def function_with_logic(*args):
+    return args
 
-# print_result = function_with_logic(None, True, 1, 's', 7.3, None, '')
-# print(print_result)
-
+print_result = function_with_logic(None, True, 1, 's', 7.3, None, '')
+print(print_result)
 
 
 # 5. Split the string and find the first letter of the word
-# def split_str(your_string):
-#     for item in your_string.split(" "):
-#         print(f"The first letter of the word '{item}', is '{item[0]}'.")
 
-# split_str("hello my name is max")
+def split_str(your_string):
+    for item in your_string.split(" "):
+        print(f"The first letter of the word '{item}', is '{item[0]}'.")
+
+split_str("hello my name is max")
 
 
 # 6. Реалізувати функцію, яка буде загорнута в декілька декораторів (декоратори потрібно написати самим)
@@ -130,6 +130,43 @@ print(calculate_sum(4, 11, 1, 20, 6))
 
 # 6. Написати декоратор, який всередині буде відловлювати помилку (реалізувати try-except statement)
 
+def handle_exceptions(func):
+    def wrapper(*args):
+        try:
+            result = func(*args)
+            print(result)
+        except Exception as e:
+            print(f"Exception occurred: {e}")
+    return wrapper
 
+@handle_exceptions
+def divide(a, b):
+    return a / b
+
+divide(0, 0)
 
 # 7. Напишіть декоратор, який записує в файл час, коли була викликана функція
+
+import time
+
+with open('Python_Advanced/args_sum_execution_time.txt', "a") as file:
+    file.write("'args_sum' function was executed on: ")
+
+def execution_time(func_to_decorate):
+    def wrapper(*args):
+        start_time = time.time()
+        start_time_readable = time.strftime("%d-%m-%Y %H:%M:%S", time.localtime(start_time))
+        result = start_time_readable
+        print(f"'args_sum' function was executed on: {start_time_readable}")
+        with open('Python_Advanced/args_sum_execution_time.txt', "a") as file:
+            file.write(str(result) + '\n')
+        return result
+
+    return wrapper
+
+@execution_time
+def args_sum(a, b):
+    return a + b
+
+args_sum(5, 3)
+file.close()
