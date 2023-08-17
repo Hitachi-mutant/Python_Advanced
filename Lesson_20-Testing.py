be25c00404d69c9dd3d834a32ccc63af304a82e0
Alex <goldskydiver@gmail.com>
2023-08-17 18:52:47 +0300

    Lesson 31 SQL Part 2 - homework is DONE

--- Lesson_20-Testing.py
+++ Lesson_20-Testing.py
@@ -4,41 +4,41 @@
 Pick your solution to one of the exercises in this module. 
 Design tests for this solution and write tests using unittest library. '''
 
-# import unittest
-# from datetime import datetime, timedelta
+import unittest
+from datetime import datetime, timedelta
 
-# class Airport:
-#     def __init__(self, airport_name, airport_type, airport_operator, location, time_zone):
-#         self.airport_name = airport_name
-#         self.airport_type = airport_type
-#         self.airport_operator = airport_operator
-#         self.location = location
-#         self.time_zone = time_zone
+class Airport:
+    def __init__(self, airport_name, airport_type, airport_operator, location, time_zone):
+        self.airport_name = airport_name
+        self.airport_type = airport_type
+        self.airport_operator = airport_operator
+        self.location = location
+        self.time_zone = time_zone
     
-#     def airport_info(self):
-#         return f'{self.airport_name} is a {self.airport_type} airport. Located in {self.location}'
+    def airport_info(self):
+        return f'{self.airport_name} is a {self.airport_type} airport. Located in {self.location}'
     
-#     def destination_point_time(self, departure_time, flight_duration, destination_time_zone):
-#         time_zone_difference = destination_time_zone - self.time_zone
-#         arrival_time = departure_time + flight_duration + time_zone_difference
-#         arrival_time_formatted = arrival_time.strftime("%H:%M")
-#         return arrival_time_formatted
+    def destination_point_time(self, departure_time, flight_duration, destination_time_zone):
+        time_zone_difference = destination_time_zone - self.time_zone
+        arrival_time = departure_time + flight_duration + time_zone_difference
+        arrival_time_formatted = arrival_time.strftime("%H:%M")
+        return arrival_time_formatted
     
-# class TestAirport(unittest.TestCase):
+class TestAirport(unittest.TestCase):
     
-#     def setUp(self):
-#         self.Dallas_Airport = Airport("Dallas Airport", "International", "Fort Worth International Airport Board", "Dallas, Texas, USA", timedelta(hours=-6))
-#         self.Miami_Airport = Airport("Miami International Airport", "International", "Miami-Dade Aviation Department", "Miami, Florida, USA", timedelta(hours=-5))
+    def setUp(self):
+        self.Dallas_Airport = Airport("Dallas Airport", "International", "Fort Worth International Airport Board", "Dallas, Texas, USA", timedelta(hours=-6))
+        self.Miami_Airport = Airport("Miami International Airport", "International", "Miami-Dade Aviation Department", "Miami, Florida, USA", timedelta(hours=-5))
     
-#     def test_airport_info(self):
-#         self.assertEqual(self.Dallas_Airport.airport_info(), 'Dallas Airport is a International airport. Located in Dallas, Texas, USA')
-#         self.assertEqual(self.Miami_Airport.airport_info(), 'Miami International Airport is a International airport. Located in Miami, Florida, USA')
+    def test_airport_info(self):
+        self.assertEqual(self.Dallas_Airport.airport_info(), 'Dallas Airport is a International airport. Located in Dallas, Texas, USA')
+        self.assertEqual(self.Miami_Airport.airport_info(), 'Miami International Airport is a International airport. Located in Miami, Florida, USA')
     
-#     def test_destination_point_time(self):
-#         self.assertEqual(self.Dallas_Airport.destination_point_time(datetime.strptime('16:00', '%H:%M'), timedelta(hours=3), timedelta(hours=-2)), '23:00')
+    def test_destination_point_time(self):
+        self.assertEqual(self.Dallas_Airport.destination_point_time(datetime.strptime('16:00', '%H:%M'), timedelta(hours=3), timedelta(hours=-2)), '23:00')
 
-# if __name__ == '__main__':
-#     unittest.main()
+if __name__ == '__main__':
+    unittest.main()
 
 
 '''Task 2 - Phonebook application testing: 
--- SQL_Tasks.sql
+++ SQL_Tasks.sql
@@ -44,3 +44,76 @@ order by count(patients.first_name)*/
 from patients JOIN province_names ON patients.province_id=province_names.province_id 
 where birth_date between '1940-01-01' and '1960-01-01' group by province_name 
 having count(patients.first_name) > 10;*/
+
+
+
+--1. Show the category_name and description from the categories table sorted by category_name.
+--select category_name, description from categories order by category_name asc
+--2. Show the product_name and unit_price from the products table sorted by unit_price in descending order.
+--select product_name, unit_price from products order by unit_price desc
+--3. Show all the contact_name, address, city of all customers which are not from 'Germany', 'Mexico', 'Spain'
+--select contact_name, address, country, city from customers WHERE country != 'Mexico' and country != 'Germany' and country != 'Spain' order by country;
+--4. Show the employee_id, order_id, customer_id, required_date, shipped_date from all orders shipped later than the required date
+--select employee_id, order_id, customer_id, required_date, shipped_date from orders where order_date between '2016-09-01' and '2016-10-29';
+--5. Show the company_name, contact_name, fax number of all customers that has a fax number.
+--select company_name, contact_name, fax from customers where fax != 'None' order by company_name;
+--6. Show the employee_id, order_id, customer_id, required_date, shipped_date from all orders where shipped_date is later than required_date.
+--select employee_id, order_id, customer_id, required_date, shipped_date from orders where shipped_date > required_date;
+--7. Show the supplier_name, contact_name, phone number of all suppliers that have a phone number.
+--select company_name, contact_name, phone from suppliers where phone != 'None';
+--8. Show the first_name, last_name, hire_date of employees hired before '2022-01-01'.
+--select first_name, last_name, hire_date from employees where hire_date < '2022-01-01';
+--9. Show the product_name, quantity_per_unit, units_in_stock of all products that have less than 10 units in stock.
+--select product_name, quantity_per_unit, units_in_stock from products where units_in_stock < 10;
+--10. Show the company_name, contact_name, contact_title of customers that have a contact_title.
+--select company_name, contact_name, contact_title from customers where contact_title != 'None';
+--11. Show the order_id, ship_country, ship_region of orders where ship_country is not 'USA' and ship_region is not empty.
+--select order_id, ship_country, ship_region from orders where ship_country != 'USA' and ship_region != 'None';
+--12. Show the employee_id, last_name, first_name of the employees who have the title 'Sales Representative'.
+--select employee_id, title, last_name, first_name from employees where title = 'Sales Representative';
+--13. List the products that were ordered more than once in a single order. 
+--Display the product name, order ID, and quantity ordered.
+
+--select products.product_name, order_details.order_ID, order_details.quantity 
+--from products INNER JOIN order_details ON products.product_id=order_details.product_id 
+--where order_details.quantity > 1 limit 60;
+
+--14. Retrieve the top 10 most ordered categories of products. 
+--Display the category name and the total quantity of products ordered within each category.
+/*select categories.category_name, order_details.quantity 
+from categories 
+JOIN products 
+ON categories.category_id=products.category_id
+join order_details 
+on products.product_id=order_details.product_id 
+order by order_details.quantity desc limit 10;*/
+
+--15. Find the employees who have never handled any orders. Display their names and hire dates.
+/*select employees.first_name, employees.last_name, employees.hire_date, orders.order_id 
+from employees
+join orders
+on employees.employee_id=orders.employee_id where orders.order_id = 'None';*/
+
+--16. Retrieve a list of orders with the customer's company name and the employee's first 
+--and last names. Include only orders placed after January 1, 2010
+/*select employees.first_name, employees.last_name, customers.company_name, orders.order_date  
+from employees 
+join orders
+on employees.employee_id=orders.employee_id 
+join customers
+on orders.customer_id=customers.customer_id 
+where orders.order_date > '2016-12-01';*/
+
+--17. Retrieve a list of customers and suppliers who are located in the same city. 
+--Display the company name and city for both customers and suppliers. 
+--If a city doesn't have any customers or suppliers, show "No Data" for the missing side.
+/*SELECT 
+    customers.customer_id AS customer_id,
+    customers.company_name AS customer_company,
+    suppliers.supplier_id AS supplier_id,
+    suppliers.company_name AS supplier_company,
+    customers.city AS city
+FROM 
+    customers
+JOIN 
+    suppliers ON customers.city = suppliers.city;*/
